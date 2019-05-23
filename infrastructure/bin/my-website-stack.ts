db06258f8f18023cca021820e46cd7fca8845eb9
Jeff Cooper <jeff@cuperman.net>
2019-05-22 17:16:04 -0700

    Base static React site with CDK

---
+++ infrastructure/bin/my-website-stack.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import { App } from "@aws-cdk/cdk";
+import { WebsiteStack } from "../lib/website-stack";
+
+const app = new App();
+new WebsiteStack(app, "MyWebsiteStack");
---
+++ infrastructure/lib/website-stack.ts
@@ -0,0 +1,9 @@
+import { Stack, Construct, StackProps } from "@aws-cdk/cdk";
+
+export class WebsiteStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+  }
+}
