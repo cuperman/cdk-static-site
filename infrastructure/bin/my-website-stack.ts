87557215298494afd231c0f222c1586b8936281a
Jeff Cooper <jeff@cuperman.net>
2019-05-22 17:54:08 -0700

    Create static website stack definition

--- infrastructure/bin/my-website-stack.ts
+++ infrastructure/bin/my-website-stack.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
-import "source-map-support/register";
-import { App } from "@aws-cdk/cdk";
-import { WebsiteStack } from "../lib/website-stack";
+import 'source-map-support/register';
+import { App } from '@aws-cdk/cdk';
+import { WebsiteStack } from '../lib/website-stack';
 
 const app = new App();
-new WebsiteStack(app, "MyWebsiteStack");
+new WebsiteStack(app, 'MyWebsiteStack', {
+  domain: 'example.com',
+  assetPath: '../frontend/build'
+});
--- infrastructure/lib/website-stack.ts
+++ infrastructure/lib/website-stack.ts
@@ -1,9 +1,73 @@
-import { Stack, Construct, StackProps } from "@aws-cdk/cdk";
+import { Stack, Construct, StackProps } from '@aws-cdk/cdk';
+import { HostedZoneProvider, AliasRecord } from '@aws-cdk/aws-route53';
+import { Certificate } from '@aws-cdk/aws-certificatemanager';
+import { Bucket } from '@aws-cdk/aws-s3';
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
+import { CloudFrontWebDistribution } from '@aws-cdk/aws-cloudfront';
+
+const CLOUDFRONT_ZONE_ID = 'Z2FDTNDATAQYW2'; // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-route53-aliastarget.html#cfn-route53-aliastarget-hostedzoneid
+
+interface WebsiteStackProps extends StackProps {
+  readonly domain: string;
+  readonly assetPath: string;
+}
 
 export class WebsiteStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: WebsiteStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const { domain, assetPath } = props;
+
+    const hostedZone = new HostedZoneProvider(this, {
+      domainName: domain
+    }).findAndImport(this, 'HostedZone');
+
+    const siteCertificate = new Certificate(this, 'SiteCertificate', {
+      domainName: domain,
+      subjectAlternativeNames: [domain]
+    });
+
+    const siteBucket = new Bucket(this, 'SiteBucket', {
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html'
+    });
+
+    new BucketDeployment(this, 'SiteDeployment', {
+      source: Source.asset(assetPath),
+      destinationBucket: siteBucket
+    });
+
+    const siteDistribution = new CloudFrontWebDistribution(this, 'SiteDistribution', {
+      aliasConfiguration: {
+        acmCertRef: siteCertificate.certificateArn,
+        names: [domain]
+      },
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: siteBucket
+          },
+          behaviors: [
+            {
+              isDefaultBehavior: true
+            }
+          ]
+        }
+      ]
+    });
+
+    new AliasRecord(this, 'SiteAlias', {
+      zone: hostedZone,
+      recordName: `${domain}.`,
+      target: {
+        asAliasRecordTarget: () => {
+          return {
+            hostedZoneId: CLOUDFRONT_ZONE_ID,
+            dnsName: siteDistribution.domainName
+          };
+        }
+      }
+    });
   }
 }
